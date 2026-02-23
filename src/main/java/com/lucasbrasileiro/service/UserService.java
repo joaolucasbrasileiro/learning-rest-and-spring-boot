6d3ff589faae0cc4aed8653ffa5ac69839fc47b7
Lucas Brasileiro <lucasbrasileiro_789@hotmail.com>
2026-02-22 21:10:02 -0300

    changing the springboot and jdk version to 3.4.0 and 21, respectly

--- src/main/java/com/lucasbrasileiro/service/UserService.java
+++ src/main/java/com/lucasbrasileiro/service/UserService.java
@@ -1,6 +1,11 @@
 package com.lucasbrasileiro.service;
 
+
+import com.lucasbrasileiro.data.dto.v1.UserDTO;
 import com.lucasbrasileiro.exception.ResourceNotFoundException;
+import static com.lucasbrasileiro.mapper.ObjectMapper.parseListObjects;
+import static com.lucasbrasileiro.mapper.ObjectMapper.parseObject;
+
 import com.lucasbrasileiro.model.User;
 import com.lucasbrasileiro.repository.UserRepository;
 import org.slf4j.Logger;
@@ -19,20 +24,23 @@ public class UserService {
     UserRepository userRepository;
 
 
-    public List<User> findAll() {
+    public List<UserDTO> findAll() {
         logger.info("Find All Users");
-        return userRepository.findAll();
+        return parseListObjects(userRepository.findAll(),  UserDTO.class);
     }
 
-    public User findById(Long id) {
+    public UserDTO findById(Long id) {
         logger.info("Find User by ID {}", id);
-        return userRepository.findById(id)
+        var entity = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User não encontrado!"));
+        return parseObject(entity, UserDTO.class);
     }
 
-    public User create(User user) {
+    public UserDTO create(UserDTO user) {
         logger.info("Create User {}", user);
-       return userRepository.save(user);
+        var entity = parseObject(user, User.class);
+
+        return parseObject(userRepository.save(entity), UserDTO.class);
     }
 
     public void delete (Long id) {
@@ -42,7 +50,7 @@ public class UserService {
         userRepository.delete(entity);
     }
 
-    public User update (User user) {
+    public UserDTO update (UserDTO user) {
         logger.info("Update User {}", user);
         User entity = userRepository.findById(user.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User não encontrado!"));
@@ -51,6 +59,7 @@ public class UserService {
         entity.setPassword(user.getPassword());
         entity.setEmail(user.getEmail());
         entity.setGender(user.getGender());
-        return userRepository.save(entity);
+
+        return parseObject(userRepository.save(entity), UserDTO.class);
     }
 }
